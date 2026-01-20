090cc57ce3a1d74b0c9ce515a2f7368a138a5a6c
Rahul G <rahulgirish110206@gmail.com>
2026-01-20 22:08:04 +0530

    added streaming
    -added function that streams to web using Flask
    -added threading for stream

--- raspberry-pi/camera.py
+++ raspberry-pi/camera.py
@@ -1,4 +1,6 @@
 import cv2
+from flask import Flask, Response
+import threading
 import config
 
 def capture_video():
@@ -27,11 +29,50 @@ def capture_video():
             print("Error: Can't receive frame.")
             break
         
-        resized_frame = cv2.resize(frame, (640, 200), interpolation=cv2.INTER_AREA)
-        cv2.imshow("Raspberry Pi Webcam", resized_frame)
+        frame = cv2.resize(frame, (config.RESIZED_WIDTH, config.RESIZED_HEIGHT), interpolation=cv2.INTER_AREA)
+        cv2.imshow("Raspberry Pi Webcam", frame)
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         
     cap.release()
     cv2.destroyAllWindows()
+    
+
+app = Flask(__name__)
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        exit()
+        
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, config.FRAME_WIDTH)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, config.FRAME_HEIGHT)
+cap.set(cv2.CAP_PROP_FPS, config.FPS)
+
+def generate_frames():
+    
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        frame = cv2.resize(frame, (config.RESIZED_WIDTH, config.RESIZED_HEIGHT))
+        ret, buffer = cv2.imencode('.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+        
+        
+@app.route('/')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def start_stream():
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    
+def stream():
+    
+    thread = threading.Thread(target=start_stream)
+    thread.daemon = True
+    thread.start()
+    print("Stream started at http://192.168.1.22:5000")
--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -1,4 +1,20 @@
-from camera import capture_video
+import time
+from camera import stream
+
 print("Raspberry Pi module is running.")
 
-capture_video()
+def main():
+    
+    stream()
+    
+    try:
+        while True:
+            time.sleep(1)
+            
+    except KeyboardInterrupt:
+        print("Stopping.")
+        
+
+if __name__ == "__main__":
+    
+    main()
